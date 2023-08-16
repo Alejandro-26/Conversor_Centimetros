a6a8116fd672eecbed6dcc6cb9b1b875f213567d
Alejo Duran <111656887+Alejandro-26@users.noreply.github.com>
2023-08-15 21:25:46 -0500

    Conversor

---
+++ Conversor/Vistas/Convertir.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm;
+        double m;
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+        private void calcula()
+        {
+            cm = Convert.ToDouble(txtCentimetros.Text);
+            m = cm / 100;
+            lblResultado.Text = m.ToString() + " m";
+        }
+
+        private void validar()
+        {
+            if (!string.IsNullOrEmpty(txtCentimetros.Text))
+            {
+                calcula();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad > 0", "Cancelar");
+            }
+        }
+
+        private void btnVolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            validar();
+        }
+    }
+}
