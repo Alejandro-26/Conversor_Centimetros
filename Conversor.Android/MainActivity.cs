efb3c065832168581d5ee7dd2389f7574802d2f5
Alejo Duran <111656887+Alejandro-26@users.noreply.github.com>
2023-08-25 14:05:25 -0500

    Cambiando icons

--- Conversor.Android/MainActivity.cs
+++ Conversor.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Conversor.Droid
 {
-    [Activity(Label = "Conversor", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Conversor", Icon = "@mipmap/IconCover", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- Conversor.Android/Resources/Resource.designer.cs
+++ Conversor.Android/Resources/Resource.designer.cs
@@ -24032,11 +24032,14 @@ namespace Conversor.Droid
 			// aapt resource value: 0x7F0C0000
 			public const int icon = 2131492864;
 			
+			// aapt resource value: 0x7F0C0002
+			public const int IconCover = 2131492866;
+			
 			// aapt resource value: 0x7F0C0001
 			public const int icon_round = 2131492865;
 			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			// aapt resource value: 0x7F0C0003
+			public const int launcher_foreground = 2131492867;
 			
 			static Mipmap()
 			{
